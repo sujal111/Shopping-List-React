f723c5da3849307a1db63fda51dfbf1dc3a67ef2
sujalmaiti <sujalmaiti123456@gmail.com>
2024-04-25 01:09:56 +0530

    added header to shopping list

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import suj from '../../assets/suj.jpeg'
+
+import './Header.css';
+
+function Header(){
+    return (
+    <div className="header-wrapper"> 
+<h1
+className='header_text'
+>Shooping List</h1>   
+<img className='header_image'
+src={suj}
+/>
+</div>
+
+    )
+}
+
+export default Header;
